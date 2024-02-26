3578f38009736504112d5234338cf20a7514c0bb
baishengye <amazong-xyb@qq.com>
2024-02-26 23:22:40 +0800

    feat:初始化lint库

---
+++ Lib-Lint-Demo/settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lib-Lint-Demo"
+include(":app")
+include(":lib_lint")
+include(":lib_lint_rules")
